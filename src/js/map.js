22713927e2ed1c3e346b324a30ffd8b3d2b31167
markGambrelPhila <118294876+markGambrelPhila@users.noreply.github.com>
2026-07-14 11:16:31 -0400

    Language updates may and july 2026

--- src/js/map.js
+++ src/js/map.js
@@ -985,13 +985,13 @@ $(function (router) {
 
     function zoningStyle(feature){
         if(feature.properties.CODE==="RSA1" || feature.properties.CODE==="RSA2" || feature.properties.CODE==="RSA3" || feature.properties.CODE==="RSA4" || feature.properties.CODE==="RSA5" || feature.properties.CODE==="RSA6"){
-            return{color: '#F8EF67'}
+            return{color: '#F4EF67'}
         }
         else if(feature.properties.CODE==="RSD1" || feature.properties.CODE==="RSD2" || feature.properties.CODE==="RSD3"){
             return{color: '#FEF5C4'}
         }
         else if(feature.properties.CODE==="RTA1" || feature.properties.CODE==="RTA2"){
-            return{color: '#CDB54F'}
+            return{color: '#CDB54E'}
         }
         else if(feature.properties.CODE==="RM1" || feature.properties.CODE==="RM2" || feature.properties.CODE==="RM3" || feature.properties.CODE==="RM4" || feature.properties.CODE==="RM5"){
             return{color: '#FCB851'}
